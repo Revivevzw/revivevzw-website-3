a6323caddc94cb21c51ba358e35d20051fdc57f8
Maarten Brysbaert <maarten.brysbaert@qite.be>
2026-02-01 22:53:15 +0100

    About page + styling changes footer

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { InfoBlock } from '../../components/info-block/info-block';
+import { SplitContentCard } from '../../components/split-content-card/split-content-card';
+import { Breadcrumbs } from '../../components/breadcrumbs/breadcrumbs';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-about',
-    imports: [],
+    imports: [InfoBlock, SplitContentCard, Breadcrumbs, RouterLink],
     templateUrl: './about.html',
     styleUrl: './about.css',
 })
