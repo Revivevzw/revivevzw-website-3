f68cf6303f77268f84a24cd0e66acf3758682b72
Maarten Brysbaert <maarten.brysbaert@qite.be>
2026-04-10 16:24:48 +0200

    Some link fixes

--- src/app/components/cta-banner/cta-banner.ts
+++ src/app/components/cta-banner/cta-banner.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-cta-banner',
-  imports: [TranslateModule],
-  templateUrl: './cta-banner.html',
-  styleUrl: './cta-banner.css',
+    selector: 'app-cta-banner',
+    imports: [TranslateModule, RouterLink],
+    templateUrl: './cta-banner.html',
+    styleUrl: './cta-banner.css',
 })
 export class CtaBanner {
 
--- src/app/pages/team/team.ts
+++ src/app/pages/team/team.ts
@@ -6,10 +6,11 @@ import { Breadcrumbs } from "../../components/breadcrumbs/breadcrumbs";
 import { InfoBlock } from "../../components/info-block/info-block";
 import { LocalizePipe } from "../../pipes/localize.pipe";
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-team',
-    imports: [SplitContentCard, Breadcrumbs, InfoBlock, LocalizePipe, TranslateModule],
+    imports: [SplitContentCard, Breadcrumbs, InfoBlock, LocalizePipe, RouterLink, TranslateModule],
     templateUrl: './team.html',
     styleUrl: './team.css',
 })
